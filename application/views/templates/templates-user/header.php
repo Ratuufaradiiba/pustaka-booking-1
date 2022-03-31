99bc8d3f33cf0f8fea8c9cf22228a0192d87ae60
Ratuufaradiiba <ratuufaradyb.05@gmail.com>
2022-03-31 12:29:21 +0700

    WP3 PERT3

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -22,16 +22,16 @@
 
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sronly">(current)</span></a>
+                    <a class="nav-item nav-link active" href="<?= base_url(); ?>"><i class="fas fw fa-home"></i> Beranda<span class="sronly"></span></a>
                     
                     <?php
                     if (!empty($this->session->userdata('email'))) { ?>
-                        <a class="nav-item nav-link" href="<?= base_url('booking'); ?> ">Booking Buku </a>
-                        <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
+                        <a class="nav-item nav-link" href="<?= base_url('booking'); ?> "> <i class="far fw fa-shopping-cart"></i> Booking Buku </a>
+                        <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>"><i class="far fw fa-address-cart"></i> Profil Saya</a>
                         <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
                     <?php } else { ?>
 
-                        <a class="nav-item nav-link" data-toggle="modal" datatarget="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
+                        <a class="nav-item nav-link" data-toggle="modal" datatarget="#daftarModal" href="#"><i class="fas fw fa-user"></i> Daftar</a>
                         <a class="nav-item nav-link" data-toggle="modal" datatarget="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
                     
                     <?php } ?>
